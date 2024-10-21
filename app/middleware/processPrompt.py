99f33f75cc29b957d1b8a1e18f3803857c7f616e
unknown <gaurav.sharma.782@my.csun.edu>
2024-10-22 05:02:51 +0530

    first

--- app/middleware/processPrompt.py
+++ app/middleware/processPrompt.py
@@ -1,12 +1,49 @@
+import os
+import openai 
+import tiktoken
+from dotenv import load_dotenv, find_dotenv
+from typing import Dict
+#_ = load_dotenv(find_dotenv()) # read local .env file
 
 
-"""
-Process prompts
-"""
+
+openai.api_key  = "sk-y_9Z1HapNzRwJgiaRkKRawrt3-8D2KA6zTFX-3t5GTT3BlbkFJ9hlUe3AqeY6agzHKcUYF9PdNVJbXFYvIDT5osvQ1oA"
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    
+
+    messages = [{"role": "user", "content": prompt}, {'role':'system', 
+ "content":"you are an AI assistant with expertise in providing important features of `description`, `product details`, and `taxonomy` from the json format provided, dont provide html tags "}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output 
+    )
+    return response.choices[0].message["content"] 
+
+def get_completion_final(prompt, model="gpt-3.5-turbo"):
+    
+
+    messages = [{"role": "user", "content": prompt}, {'role':'system', 
+ "content":"you are an AI assistant with expertise in providing response in detail in well structured format focuing on `description`, `product details`, and `taxonomy` from the json format provided, dont provide html tags "}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output 
+    )
+    return response.choices[0].message["content"]
+
 
 def promptResponse(json_string) -> str:
-    response = f" Here are the product details: {json_string}"
     
-    return response
+    x=get_completion(json_string,model="gpt-3.5-turbo")
+    
+    return get_completion_final(x,model="gpt-3.5-turbo")
+
+
+    
+
+
+    
 
 
